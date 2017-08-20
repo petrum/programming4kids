3cd0fad617eabda7db9171555786c5d971cd2d3c
Petru Marginean <petru.marginean@gmail.com>
2017-08-20 13:21:48 -0400

    misc

--- build.sh
+++ build.sh
@@ -11,7 +11,12 @@ git push origin master
 #git checkout gh-pages
 #git merge master
 #git commit -ammisc
+
+# do it w/o checkout in gh-pages (as it cause reload in jupyter notebook)
+#https://stackoverflow.com/questions/3216360/merge-update-and-pull-git-branches-without-using-checkouts
 git fetch origin master:gh-pages
+
+
 git push origin gh-pages
 #git checkout master
 
