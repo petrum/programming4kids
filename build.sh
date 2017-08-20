b3e2f4e4e0316e7ee419a3550c689cf56f76049a
Petru Marginean <petru.marginean@gmail.com>
2017-08-20 10:21:19 -0400

    misc

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+jupyter-nbconvert --to slides programming4kids02.ipynb
+git commit -ammisc
+git push origin gh-pages
+
+#https://petrum.github.io/programming4kids/programming4kids02.slides.html#/
+
