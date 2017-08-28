88dbf05c93519c0eac60fdc6d8e062e7efc1de09
Petru Marginean <petru.marginean@gmail.com>
2017-08-27 22:27:01 -0400

    misc

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 git pull
 git add *.ipynb
-jupyter-nbconvert --to slides programming4kids*.ipynb --reveal-prefix /home/petrum/reveal.js
+jupyter-nbconvert --to slides programming4kids*.ipynb
 #sed -i s/simple.css/sky.css/g *.html
 
 git add *.slides.html
